44a0b730a6dd4f826eb50d1d920f7bf57b8cdc19
Jae-Holderby <jae.holderby@gmail.com>
2017-10-06 12:02:11 -0600

    😃  refactored cards and moved findParty into it's own function

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ function submitAddress(event){
 }
 
 function createStateDropdown() {
-for(var i = 0; i < states.length; i++) {
-      $("select").append("<option>" + states[i] + "</option>")
-    }
+ states.forEach(state => {
+   $("select").append("<option>" + state + "</option>")
+ })
 }
 
 function displayLoading(){
@@ -86,43 +86,49 @@ headers: {"X-API-Key": "h8MbAqUKVc70UFTS0O4qA7kZ1a5wiIR96PSUQsOm"}
 
 function showMyHouseRep(houseRepInfo) {
   hideLodaing()
-  var name = houseRepInfo.results[0].first_name + " " + houseRepInfo.results[0].last_name
-  var image = "https://theunitedstates.io/images/congress/225x275/"+ houseRepInfo.results[0].member_id
+  var houseRep = houseRepInfo.results[0]
+  var name = houseRep.first_name + " " + houseRep.last_name
+  var image = "https://theunitedstates.io/images/congress/225x275/"+ houseRep.member_id
+  var district = houseRep.roles[0].district
+  var url = houseRep.url
+  var partyVotePercentage = houseRep.roles[0].votes_with_party_pct
+  var missedVotePercentage = houseRepInfo.results[0].roles[0].missed_votes_pct
+  var phoneNumber = houseRepInfo.results[0].roles[0].phone
 
-  var party = findParty()
+  var party = findParty(houseRepInfo)
 
-  function findParty() {
-    if(houseRepInfo.results[0].current_party === "D") {
-      return party = "Democrat"
-  } else if (houseRepInfo.results[0].current_party === "R"){
-      return party = "Republican"
-    } else {
-      return party = "Independant"
-    }
-  }
-
-  $(".title").append(`<h2> District ${houseRepInfo.results[0].roles[0].district} </h2>`)
+  $(".title").append(`<h2> District ${district} </h2>`)
 
   $('.cards').append(
     `<div class='card col-sm-7 col-md-5 col-lg-3 ${party}'>
       <div class='card-header align-self-center'> <h4> Representative <br> ${name}</h4> </div>
-        <a target='blank' href=${houseRepInfo.results[0].url} >
+        <a target='blank' href=${url} >
           <img class='card-img-top img-thumbnail' src=' ${image}.jpg' alt='Senator's Photo'>
         </a>
         <div class='card-block'>
         <p class='card-text'>
           <h5> ${party} </h5> <br>
           <strong> Party line vote percentage: </strong>
-          <br>    ${houseRepInfo.results[0].roles[0].votes_with_party_pct} %
+          <br>    ${partyVotePercentage} %
           <br> <strong> Missed vote percentage: </strong> <br>
-          ${houseRepInfo.results[0].roles[0].missed_votes_pct} %
+          ${missedVotePercentage} %
           <br> <strong> Phone Number: </strong> <br>
-          <a href='tel:${houseRepInfo.results[0].roles[0].phone}'>${houseRepInfo.results[0].roles[0].phone}<a/> <br> <br>
-          <a target='blank' href=${houseRepInfo.results[0].url} > ${houseRepInfo.results[0].last_name}.house.gov
+          <a href='tel:${phoneNumber}'>${phoneNumber}<a/> <br> <br>
+          <a target='blank' href=${url} > ${houseRepInfo.results[0].last_name}.house.gov
           </a>
         </p>
       </div>
     </div>`)
+}
+
+function findParty(repInfo) {
+  if(repInfo.results[0].current_party === "D") {
+    return party = "Democrat"
+} else if (repInfo.results[0].current_party === "R"){
+    return party = "Republican"
+  } else {
+    return party = "Independant"
+  }
 }
 
 function findSenateReps(senateRepInfo) {
@@ -155,37 +161,33 @@ function getSenateRep2Info(senateRepInfo) {
 }
 
 function showMySenateRep1(senateInfo) {
-  var name = senateInfo.results[0].first_name + " " + senateInfo.results[0].last_name
-  var image = "https://theunitedstates.io/images/congress/225x275/"+ senateInfo.results[0].member_id
+  var senateRep = senateInfo.results[0]
+  var name = senateRep.first_name + " " + senateRep.last_name
+  var image = "https://theunitedstates.io/images/congress/225x275/"+ senateRep.member_id
+  var district = senateRep.roles[0].district
+  var url = senateRep.url
+  var partyVotePercentage = senateRep.roles[0].votes_with_party_pct
+  var missedVotePercentage = senateRep.roles[0].missed_votes_pct
+  var phoneNumber = senateRep.roles[0].phone
 
-  var party = findParty()
-
-  function findParty() {
-    if(senateInfo.results[0].current_party === "D") {
-      return party = "Democrat"
-  } else if (senateInfo.results[0].current_party === "R"){
-      return party = "Republican"
-    } else {
-      return party = "Independant"
-    }
-  }
+  var party = findParty(senateInfo)
 
   $('.cards').append(
     `<div class='card col-sm-7 col-md-5 col-lg-3 ${party}'>
       <div class='card-header align-self-center'> <h4> Senator <br> ${name}</h4> </div>
-      <a target='blank' href=${senateInfo.results[0].url} >
+      <a target='blank' href=${url} >
         <img class='card-img-top img-thumbnail' src=' ${image}.jpg' alt='Senator's Photo'>
         </a>
         <div class='card-block'>
         <p class='card-text'>
           <h5> ${party} </h5> <br>
           <strong> Party line vote percentage: </strong>
-          <br>  ${senateInfo.results[0].roles[0].votes_with_party_pct} %
+          <br>  ${partyVotePercentage} %
           <br> <strong> Missed vote percentage: </strong> <br>
-          ${senateInfo.results[0].roles[0].missed_votes_pct} %
+          ${missedVotePercentage} %
           <br> <strong> Phone Number: </strong> <br>
-            <a href='tel:${senateInfo.results[0].roles[0].phone}'>${senateInfo.results[0].roles[0].phone}<a/> <br> <br>
-          <a target='blank' href=${senateInfo.results[0].url} > ${senateInfo.results[0].last_name}.house.gov
+            <a href='tel:${phoneNumber}'>${phoneNumber}<a/> <br> <br>
+          <a target='blank' href=${url} > ${senateInfo.results[0].last_name}.senate.gov
           </a>
         </p>
       </div>
@@ -193,37 +195,33 @@ function showMySenateRep1(senateInfo) {
 }
 
 function showMySenateRep2(senateInfo) {
-  var name = senateInfo.results[0].first_name + " " + senateInfo.results[0].last_name
-  var image = "https://theunitedstates.io/images/congress/225x275/"+ senateInfo.results[0].member_id
+  var senateRep = senateInfo.results[0]
+  var name = senateRep.first_name + " " + senateRep.last_name
+  var image = "https://theunitedstates.io/images/congress/225x275/"+ senateRep.member_id
+  var district = senateRep.roles[0].district
+  var url = senateRep.url
+  var partyVotePercentage = senateRep.roles[0].votes_with_party_pct
+  var missedVotePercentage = senateRep.roles[0].missed_votes_pct
+  var phoneNumber = senateRep.roles[0].phone
 
-  var party = findParty()
-
-  function findParty() {
-    if(senateInfo.results[0].current_party === "D") {
-      return party = "Democrat"
-  } else if (senateInfo.results[0].current_party === "R"){
-      return party = "Republican"
-    } else {
-      return party = "Independant"
-    }
-  }
+  var party = findParty(senateInfo)
 
   $('.cards').append(
     `<div class='card col-sm-7 col-md-5 col-lg-3 ${party}'>
       <div class='card-header align-self-center'> <h4> Senator <br> ${name}</h4> </div>
-      <a target='blank' href=${senateInfo.results[0].url} >
+      <a target='blank' href=${url} >
         <img class='card-img-top img-thumbnail' src=' ${image}.jpg' alt='Senator's Photo'>
         </a>
         <div class='card-block'>
         <p class='card-text'>
           <h5> ${party} </h5> <br>
           <strong> Party line vote percentage: </strong>
-          <br>  ${senateInfo.results[0].roles[0].votes_with_party_pct} %
+          <br>  ${partyVotePercentage} %
           <br> <strong> Missed vote percentage: </strong> <br>
-          ${senateInfo.results[0].roles[0].missed_votes_pct} %
+          ${missedVotePercentage} %
           <br> <strong> Phone Number: </strong> <br>
-            <a href='tel:${senateInfo.results[0].roles[0].phone}'>${senateInfo.results[0].roles[0].phone}<a/> <br> <br>
-          <a target='blank' href=${senateInfo.results[0].url} > ${senateInfo.results[0].last_name}.house.gov
+            <a href='tel:${phoneNumber}'>${phoneNumber}<a/> <br> <br>
+          <a target='blank' href=${url} > ${senateInfo.results[0].last_name}.senate.gov
           </a>
         </p>
       </div>
